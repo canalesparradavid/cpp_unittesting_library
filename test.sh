454af8bf0af5774fbb6209cd0cd71c2cb31983cb
David Canales <canalesparradavid@gmail.com>
2023-12-01 12:14:06 +0100

    Añadidas variables de directorios de compilacion

--- test.sh
+++ test.sh
@@ -4,36 +4,40 @@
 # PLEASE USE THE COMMAND dos2unix test.sh TO
 # FORMAT IT TO UNIX.
 
+SRC_DIRECTORY="src"
+TEST_DIRECTORY="test"
+TEST_TMP_DIRECTORY=$TEST_DIRECTORY"_tmp"
+
 date > test.log
 
 # Creo el directorio base de el testeo
-mkdir -p test_tmp
+mkdir -p $TEST_TMP_DIRECTORY
 
 #Compilo la clase de Test
-gcc test/Test.cpp -c -o test_tmp/Test.o -w
+gcc $TEST_DIRECTORY/Test.cpp -c -o $TEST_TMP_DIRECTORY/Test.o -w
 
 #Compilo los fichero a testear
-DEPENDENCIES=$(tree -fi src | grep .cpp)
+DEPENDENCIES=$(tree -fi $SRC_DIRECTORY | grep .cpp)
 for FILE in $DEPENDENCIES; do
     NAME_NO_EXTENSION=$(echo $FILE | awk -F/ '{print $NF}')
     NAME_NO_EXTENSION=$(echo $NAME_NO_EXTENSION | cut -d '.' -f 1)
-    gcc $FILE -c -o test_tmp/$NAME_NO_EXTENSION.o -fpermissive -w
+    gcc $FILE -c -o $TEST_TMP_DIRECTORY/$NAME_NO_EXTENSION.o -fpermissive -w
 done;
 
 #Compilo los test y los ejecuto
 TOTAL_FAILURES=0
 FAILED_TESTS=""
-TESTS=$(tree -fi test | grep -E [^/]Test.cpp)
+TESTS=$(tree -fi $TEST_DIRECTORY | grep -E [^/]Test.cpp)
 for FILE in $TESTS; do
     NAME_NO_EXTENSION=$(echo $FILE | cut -d '.' -f 1)                       # Quito la extension del fichero
     NAME_NO_EXTENSION=$(echo $NAME_NO_EXTENSION | cut -d '/' -f 2-)         # Quito la primera parte de la ruta
     FILE_PATH=$(echo "/"$NAME_NO_EXTENSION | rev | cut -d '/' -f 2- | rev)     # Almaceno la subruta
 
-    mkdir -p ./test_tmp/$FILE_PATH
+    mkdir -p ./$TEST_TMP_DIRECTORY/$FILE_PATH
 
     if [ $NAME_NO_EXTENSION != 'Test' ]; then
-        g++ test/$NAME_NO_EXTENSION.cpp test_tmp/*.o -o test_tmp/$NAME_NO_EXTENSION -w;
-        ./test_tmp/$NAME_NO_EXTENSION $NAME_NO_EXTENSION >> test.log;
+        g++ $TEST_DIRECTORY/$NAME_NO_EXTENSION.cpp $TEST_TMP_DIRECTORY/*.o -o $TEST_TMP_DIRECTORY/$NAME_NO_EXTENSION -w;
+        ./$TEST_TMP_DIRECTORY/$NAME_NO_EXTENSION $NAME_NO_EXTENSION >> test.log;
         FAILURES=$?
         [ $FAILURES -ne 0 ] && FAILED_TESTS="$FAILED_TESTS $NAME_NO_EXTENSION"
         TOTAL_FAILURES=$((FAILURES + $FAILURES))
@@ -42,4 +46,4 @@ done;
 
 cat test.log
 
-rm -rf test_tmp
+rm -rf $TEST_TMP_DIRECTORY
